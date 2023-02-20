08c229d2e7f147cb9445549d49cfdae246b0f9e7
MiguelCastro9 <miguel.castro9@hotmail.com>
2023-02-20 14:31:19 -0300

    adicionando enpoint de salvar paciente

--- src/main/java/com/api/controller/PacienteController.java
+++ src/main/java/com/api/controller/PacienteController.java
@@ -1,15 +1,20 @@
 package com.api.controller;
 
+import com.api.dto.PacienteRequestDto;
 import com.api.dto.PacienteResponseDto;
+import com.api.model.PacienteModel;
 import com.api.service.PacienteService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +38,13 @@ public class PacienteController {
                 pacienteService.listar().stream().map(paciente
                         -> PacienteResponseDto.converterEntidadeParaPacienteDto(paciente))
                         .collect(Collectors.toList()), HttpStatus.OK);
+    }
+    
+    @PostMapping
+    @Operation(description = "Salvar")
+    public ResponseEntity<PacienteResponseDto> salvar(@Valid @RequestBody PacienteRequestDto pacienteRequestDto) {
+        
+        PacienteModel pacienteModel = pacienteService.salvar(pacienteRequestDto.converterPacienteDtoParaEntidade());
+        return new ResponseEntity<PacienteResponseDto>(PacienteResponseDto.converterEntidadeParaPacienteDto(pacienteModel), HttpStatus.CREATED);
     }
 }
