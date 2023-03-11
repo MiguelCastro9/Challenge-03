14434c69b56c72a098acabee0a14e139bc7f5ad4
MiguelCastro9 <miguel.castro9@hotmail.com>
2023-03-11 16:44:20 -0300

    configurando componentes, rotas e bootstrap

--- api-web/src/app/app.module.ts
+++ api-web/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
